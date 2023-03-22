781d0c5e09f1f3dc0eda4bec5e6b117795392c92
zakariahma <zakariahmamed@gmail.com>
2023-03-22 22:24:07 +0000

    task

---
+++ mars_exploration/mars_exploration.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'marsExploration' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+function marsExploration(s) {
+    // Write your code here
+    let occ = s.length / 3;
+    let original = 'SOS'.repeat(occ);
+    let i = 0;
+    let changed = 0;
+    while (i < s.length) {
+        if (s[i] !== original[i]) {
+            changed += 1;
+        }
+        i += 1;
+    }
+    return changed;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = marsExploration(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
